5d688c440456162a2dda97e2feece5f04e4aa0bd
Aamer4ui <aamersohelansari@gmail.com>
2020-11-20 14:59:47 +0530

    practice

---
+++ src/components/Home.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div className="App">
+    
+    <h2><s>Learing the React from the beginer</s></h2>
+      <h2><s>Learing the React from the beginer</s></h2>
+      <h2><s>Learing the React from the beginer</s></h2>
+      <h2><s>Learing the React from the beginer</s></h2>
+      <h2><s>Learing the React from the beginer</s></h2>
+      <h3>
+        <i>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+          architecto ab provident nostrum totam doloribus dolorum porro
+          explicabo commodi quidem aliquid ex recusandae adipisci, error
+          repellat sunt qui iste blanditiis.
+        </i>
+      </h3><br/>
+
+      <h3>
+        <mark>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi
+          explicabo, corrupti vitae quibusdam nihil obcaecati necessitatibus
+          unde ullam reiciendis dignissimos.
+        </mark>
+      </h3>
+      <br/>
+      <a href="#home">Home</a><br/>
+    <a href="#library">Library</a><br/>
+    <a href="#sport">Sport</a><br/>
+    <a href="#study room">Study Room</a><br/>
+    
+    </div>
+  );
+};
